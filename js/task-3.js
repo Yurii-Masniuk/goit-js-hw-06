93b9a098010b2302534ec982a02544cf767f75b4
Yurii <yerikstar@gmail.com>
2025-07-03 20:53:12 +0300

    add task-3

---
+++ js/task-3.js
@@ -0,0 +1,19 @@
+class StringBuilder {
+    #value;
+    constructor(value) {
+        this.#value = value;
+    }
+    getValue() { };
+    padEnd(str) { };
+    padStart(str) { };
+    padBoth(str) { };
+}
+
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
